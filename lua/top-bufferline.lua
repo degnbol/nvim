431915c262d27893741b4b10a1d731a5a05a129b
Christian Madsen <christian.degnbol@gmail.com>
2022-02-02 11:23:17 +0000

    fix bufferline offset and move with <leader>#buffer

--- lua/top-bufferline.lua
+++ lua/top-bufferline.lua
@@ -1,7 +1,8 @@
 require "bufferline".setup {
     options = {
-        offsets = {{filetype = "NvimTree", text = "", padding = 1}},
-        indicator_icon = "", -- no line or other indicator in front of selected tab
+        numbers = "ordinal", -- can be customized to show both ordinal number and buffer id (see readme)
+        offsets = {{filetype = "NvimTree"}}, -- moves bufferline to the right when tree view is open
+        indicator_icon = " ", -- no line or other indicator in front of selected tab
         buffer_close_icon = "",
         modified_icon = "",
         close_icon = "",
@@ -10,7 +11,7 @@ require "bufferline".setup {
         max_name_length = 25,
         max_prefix_length = 20,
         tab_size = 25,
-        show_tab_indicators = false,
+        show_tab_indicators = true,
         enforce_regular_tabs = false, -- enforce same size tabs
         view = "multiwindow",
         show_buffer_close_icons = false,
@@ -22,3 +23,13 @@ require "bufferline".setup {
 -- move between tabs
 vim.api.nvim_set_keymap("n", "<TAB>", [[<Cmd>BufferLineCycleNext<CR>]], {silent=true})
 vim.api.nvim_set_keymap("n", "<S-TAB>", [[<Cmd>BufferLineCyclePrev<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>1", [[<Cmd>BufferLineGoToBuffer 1<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>2", [[<Cmd>BufferLineGoToBuffer 2<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>3", [[<Cmd>BufferLineGoToBuffer 3<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>4", [[<Cmd>BufferLineGoToBuffer 4<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>5", [[<Cmd>BufferLineGoToBuffer 5<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>6", [[<Cmd>BufferLineGoToBuffer 6<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>7", [[<Cmd>BufferLineGoToBuffer 7<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>8", [[<Cmd>BufferLineGoToBuffer 8<CR>]], {silent=true})
+vim.api.nvim_set_keymap("n", "<leader>9", [[<Cmd>BufferLineGoToBuffer 9<CR>]], {silent=true})
+
