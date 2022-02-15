f8942f9fe161d69aa3e99765ac0d6df2e485c5b4
Christian Madsen <christian.degnbol@gmail.com>
2022-02-15 18:07:41 +0000

    use radian instead of R now that we fixed bracketed paste and since it handles those

--- lua/kitty.lua
+++ lua/kitty.lua
@@ -56,6 +56,7 @@ local filetype2command = {
     python="ipython",
     julia="julia",
     -- kitty command will not have access to default setup so doesn't know where R is.
+    -- vanilla r console doesn't handle bracketed paste. Maybe there is a setting but radian works great.
     r="radian --r-binary /Library/Frameworks/R.framework/Resources/R",
     lua="lua",
 }
