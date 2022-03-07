f94f6ba3558855871f9a24c5b9463ff3789c2a49
Christian Madsen <christian.degnbol@gmail.com>
2022-03-07 17:56:09 +0000

    disable signature help pop up

--- lua/coq-nvim.lua
+++ lua/coq-nvim.lua
@@ -6,7 +6,8 @@ vim.g.coq_settings = {
     limits = {
         completion_auto_timeout = 1.0,
         completion_manual_timeout = 3.0
-    }
+    },
+    ["signature.target"] = "echo" -- disable signature help popup by default https://github.com/neoclide/coc.nvim/discussions/2778
 }
 
 vim.defer_fn(function() vim.opt.completeopt = 'menuone,noinsert' end, 1000) -- has to be deferred since COQ changes this setting
