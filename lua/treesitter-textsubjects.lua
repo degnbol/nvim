022e0163d311a2b536ff39187a9471d71097c8f7
Christian Madsen <christian.degnbol@gmail.com>
2022-04-19 19:11:18 +1000

    try out textsubjects to select code tree with . and ,

---
+++ lua/treesitter-textsubjects.lua
@@ -0,0 +1,12 @@
+#!/usr/bin/env lua
+require('nvim-treesitter.configs').setup {
+    textsubjects = {
+        enable = true,
+        prev_selection = ',', -- (Optional) keymap to select the previous selection
+        keymaps = {
+            ['.'] = 'textsubjects-smart',
+            [';'] = 'textsubjects-container-outer',
+            ['i;'] = 'textsubjects-container-inner',
+        },
+    },
+}
