ad93fed5eb082423e79c387fdc94ed04ade489e2
Christian Madsen <christian.degnbol@gmail.com>
2022-04-19 16:29:12 +1000

    markdown comment allows smooth continuation

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,3 +2,9 @@
 -- The default is html comments <!-- ... -->.
 -- For making tex documents with the markdown package it is possible to ignore latex comments, hence:
 vim.api.nvim_buf_set_option(0, "commentstring", "% %s")
+-- c=continue comment when wrapping. Should already be enabled.
+-- r=with enter in insert mode
+-- o=with o/O in normal mode
+vim.opt.formatoptions:append "cro"
+-- this also had to be set for the above to understand how a comment line looks.
+vim.opt.comments = ":%"
