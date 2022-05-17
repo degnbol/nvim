5005801b21a92e629c33f439632b12ca4e2011be
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-17 18:21:35 +1000

    smartindent instead of indentexpr for julia

--- ftplugin/julia.lua
+++ ftplugin/julia.lua
@@ -3,3 +3,16 @@ vim.api.nvim_buf_set_option(0, "commentstring", "# %s")
 -- remove o, we want to continue comments while editing them only (r).
 -- no t and having c+a means only comments are autoformatted.
 vim.opt.formatoptions = "jwcrqla"
+-- when treesitter runs for julia the indent is wrong after inline for loop,
+-- but setting the indentexpr to the one from treesitter it works, but then it
+-- breaks indents after end of function.
+-- The problem with indent happening when writing a bracket is actually due to
+-- indentkeys which are a set of keys that when written will trigger autoindent
+-- of a line. Setting the indent of a line is ALWAYS done by calling
+-- indentexpr which is GetJuliaIndent() by default. 
+-- Setting the treesitter config enable flag sets indentexpr=nvim_treesitter#indent()
+-- I think I finally found the solution to fix indent for julia.
+-- Simply use the builtin smartindent instead of indentexpr.
+vim.opt.indentexpr = ""
+vim.opt.smartindent = true
+
--- notes.md
+++ notes.md
@@ -4,5 +4,5 @@ Broadly
  - ftplugin/
  - plugins has a similar structure so may run code at all kinds of timings.
    See their code in `~/.local/share/nvim/site/pack/packer/start/`
- - after/
- - after/syntax/
+ - seems after/ is never run?
+ - after/.../
