803929258ae989b193de917982e9f26fc4ee73e8
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-21 22:22:35 +1000

    column alignment in tsv

---
+++ ftplugin/tsv.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/env lua
+api = vim.api
+opt = vim.opt
+
+function Vartabstop()
+    local vartabstop = {}
+    
+    -- first take values from header, so we don't have to check for nil for every 
+    -- line.
+    local line = api.nvim_buf_get_lines(0, 0, 1, true)[1]
+    for field in line:gmatch('[^\t]+') do
+        table.insert(vartabstop, field:len())
+    end
+    
+    -- iterate each line
+    local lines = api.nvim_buf_get_lines(0, 1, -1, true)
+    for _, line in ipairs(lines) do
+        local i = 1
+        for field in line:gmatch('[^\t]+') do
+            vartabstop[i] = math.max(vartabstop[i], field:len())
+            i=i+1
+        end
+    end
+    
+    -- add 1 minimum space
+    for i, v in ipairs(vartabstop) do
+        vartabstop[i] = v+1
+    end
+    
+    opt.vartabstop = vartabstop
+end
+
+-- run automatically sometimes. Maybe add after making changes, e.g. leaving 
+-- insert mode or TextChanged(I) event(s).
+api.nvim_create_autocmd(
+    {"BufEnter", "BufWritePost"},
+    {pattern="*.tsv", callback=Vartabstop}
+)
+
