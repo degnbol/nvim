0483fafc9ff2942f5dfb5452e3f3fe704e6aa6d2
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-06-21 14:55:37 +1000

    fo-=a default

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,4 +1,5 @@
 #!/usr/bin/env lua
 -- remove o, we want to continue comments while editing them only (r).
--- no t and having c+a means only comments are autoformatted.
-vim.opt.formatoptions = "jwcrqla"
+-- no t and having c+a means only comments are autoformatted. 
+-- However, a made comment reformat slow, so don't use by default.
+vim.opt.formatoptions = "jwcrql"
