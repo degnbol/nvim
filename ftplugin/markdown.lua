a272ab357b77101968c37155035c5d13762146f3
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-18 21:11:24 +1000

    try less auto formatting and autoindent on

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -7,12 +7,13 @@ vim.api.nvim_buf_set_option(0, "commentstring", "% %s")
 -- r=with enter in insert mode. Should already be enabled.
 -- o=with o/O in normal mode. Not sure if I want this.
 -- a=auto format by default
-opt.formatoptions:append "cra"
+-- v=inserted now, don't edit other lines.
+opt.formatoptions:append "crav"
 -- this also had to be set for the above to understand how a comment line looks.
 -- the b means the % should always be followed by a blank, i.e. space
 opt.comments = "b:%"
--- ai is useful for lists but a problem if I try to indent start of sentence, then the next lines are also indented.
-opt.autoindent = false
+-- ai is necessary for lists but a problem if I try to indent start of sentence, then the next lines are also indented.
+-- opt.autoindent = false
 -- since we are actively using the textwidth in markdown then the moving of the window is just annoying for a slim window
 opt.sidescrolloff = 0
 
