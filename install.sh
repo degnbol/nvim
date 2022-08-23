4bbfac9411ab60d38782c89e6e3238c80128133d
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-23 20:05:09 +1000

    vimtex sync with skim

--- install.sh
+++ install.sh
@@ -63,4 +63,12 @@ pip install jupytext
 # viewer, so that we are at the same location in the document whenever it gets 
 # changed. It also auto opens the pdf, and auto updates without having to focus the app first.
 brew install skim
+echo "Open skim and customize bar to your liking, e.g. hide text and show toggle pane buttons."
+# https://dr563105.github.io/blog/skim-vimtex-setup/
+echo "Go to sync settings and put"
+echo "Preset -> Custom"
+echo "Command -> nvim"
+echo "Arguments -> --headless -c \"VimtexInverseSearch %line '%file'\""
+echo "Shift+Cmd+click now moves cursor in neovim."
+
 
--- lua/vimtex.lua
+++ lua/vimtex.lua
@@ -1,10 +1,17 @@
 #!/usr/bin/env lua
+g = vim.g
 
 -- try out skim, otherwise preview would open.
-vim.g.vimtex_view_method = 'skim'
+g.vimtex_view_method = 'skim'
+-- inspiration from https://dr563105.github.io/blog/skim-vimtex-setup/
+-- forward search after every successful compilation
+g.vimtex_view_skim_sync = true
+-- change focus to skim after command `:VimtexView` is given
+g.vimtex_view_skim_activate = true
+
 -- don't show log for every compilation.
-vim.g.vimtex_quickfix_mode = 0
-vim.g.vimtex_log_ignore = {
+g.vimtex_quickfix_mode = 0
+g.vimtex_log_ignore = {
     "Underfull",
     "Overfull",
 }
