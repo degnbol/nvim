2e4d58ce85b89e0323bc1716f928ac93dd6c1b03
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-06 17:07:30 +1000

    stop annoying errors in julia caused by unicode latex part of julia-vim

--- lua/julia.lua
+++ lua/julia.lua
@@ -1,5 +1,8 @@
 local g = vim.g
 
+-- this was necessary, random lhs rhs messages was appearing 
+g.latex_to_unicode_tab = "off"
+
 -- auto insert latex if moving on with e.g. space or other writing that is not part of a unicode char
-g.latex_to_unicode_auto = true
+-- g.latex_to_unicode_auto = true
 
