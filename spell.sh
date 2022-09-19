c78f2fdb090c7dcb0f140b4cece58d76b3dea460
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-19 17:34:51 +1000

    install danish vim spell

--- spell.sh
+++ spell.sh
@@ -3,4 +3,7 @@
 mkdir -p spell
 aspell -d en dump master | aspell -l en expand > spell/en.dic
 aspell -d da dump master | aspell -l da expand > spell/da.dic
+# if `set spelllang=da` or `set spelllang=en,da` then install danish understood by vim spell with
+nvim +'set spell|q'
+# then follow prompt and install danish utf8
 
