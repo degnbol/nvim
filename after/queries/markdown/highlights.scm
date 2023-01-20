4c9279326a077093b20ea8af81688ad68f5c982b
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-01-20 14:08:59 +1100

    slightly better syntax

---
+++ after/queries/markdown/highlights.scm
@@ -0,0 +1,25 @@
+; capture different levels of headings
+(atx_heading
+  (atx_h1_marker)
+  heading_content: (inline) @text.title1)
+
+(atx_heading
+  (atx_h2_marker)
+  heading_content: (inline) @text.title2)
+
+(atx_heading
+  (atx_h3_marker)
+  heading_content: (inline) @text.title3)
+
+(atx_heading
+  (atx_h4_marker)
+  heading_content: (inline) @text.title4)
+
+(atx_heading
+  (atx_h5_marker)
+  heading_content: (inline) @text.title5)
+
+(atx_heading
+  (atx_h6_marker)
+  heading_content: (inline) @text.title6)
+
---
+++ after/queries/markdown/injections.scm
@@ -0,0 +1,16 @@
+; I want to use bash by default in code blocks in markdown but use the 
+; "info_string" if given to set the language.
+; So, check fenced_code_block not having an "info_string" subnode.
+; Sounds like "negated fields" but I might be confused about field vs child node.
+; https://tree-sitter.github.io/tree-sitter/using-parsers#pattern-matching-with-queries
+; The solution here works by requiring the specified exact order of child nodes (by using dot), 
+; which means there is no other child node allowed between, and that is where 
+; the info_string would otherwise go.
+(fenced_code_block
+  .
+  (fenced_code_block_delimiter)
+  .
+  (block_continuation)
+  .
+  (code_fence_content) @bash
+  )
