7385bf77b15a905e663cb971bad7a4ef1a5459de
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-04-18 23:08:46 +1000

    imap ctrl+shift+k to access signature help

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -9,3 +9,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- signature help is naturally bound to ctrl+k in normal mode.
+-- Access it in insert mode with a shift added, since ctrl+k is already bound 
+-- to jump to next snippet.
+vim.keymap.set("i", "<C-S-k>", vim.lsp.buf.signature_help)
+
