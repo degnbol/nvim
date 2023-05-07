55939995b0e9fc11cba7afd9247251250dc64b0a
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-05-07 22:26:32 +1000

    correct cursor placement on line with \&

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -39,29 +39,43 @@ local function in_env(name)
     return (is_inside[1] > 0 and is_inside[2] > 0)
 end
 
+-- return tex table column 0-index by counting (unescaped) ampersands (&) before cursor "column" c.
+local function getCurrentColumn(c)
+    local line = vim.api.nvim_get_current_line()
+    return #line:sub(1, c):gsub('\\&', ''):gsub('[^&]', '')
+end
+function texCol()
+    return getCurrentColumn(vim.api.nvim_win_get_cursor(0)[2])
+end
+
 -- inspired by https://gist.github.com/tpope/287147
 vim.keymap.set("i", "&", function()
     local r, c = unpack(vim.api.nvim_win_get_cursor(0))
     
     -- -1 since nvim_win_get_cursor is (1,0)-indexed and nvim_buf_set_text is 0-indexed.
     if not in_env("table") or vim.api.nvim_buf_get_text(0, r-1, c-1, r-1, c, {})[1] == "\\" then
-        -- just write a regular &
+        -- just write a regular & and exit
         vim.api.nvim_buf_set_text(0, r-1, c, r-1, c, {'&'})
         vim.api.nvim_win_set_cursor(0, {r, c+1})
         return
     end
 
     vim.api.nvim_buf_set_text(0, r-1, c, r-1, c, {'& '})
-    c = c + 2
+    local column = getCurrentColumn(c + 2)
     
-    local line = vim.api.nvim_get_current_line()
-    local column = #line:sub(1, c):gsub('[^&]', '')
     cmd [[silent! exe "normal \<Plug>alignTable"]]
-    vim.api.nvim_win_set_cursor(0, {r, 0})
-    vim.fn.search(('[^&]*&'):rep(column), 'ce', r)
-    vim.api.nvim_win_set_cursor(0, {r, api.nvim_win_get_cursor(0)[2]+2})
-    -- clear message area. Needed in combination with silent! above to not see any prints.
+    
+    local line = vim.fn.getline(r)
+    -- replace escaped ampersands with something else so they don't get counted
+    _, c = line:gsub('\\&', '  '):find(('[^&]*&'):rep(column))
+    vim.api.nvim_win_set_cursor(0, {r, c+1})
+    -- Hacks to clear message area.
+    -- Needed in combination with silent! above to not see any prints.
     print " "
     cmd "echo ' '"
 end, {remap=false, silent=true, buffer=true})
+
+
+
+
 
