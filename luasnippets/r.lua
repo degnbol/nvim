10e15fd316629e9ca9a7cad61de0079e7d0f0676
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-05-26 14:00:24 +1000

    DT update syntax + ggplot anno

--- luasnippets/r.lua
+++ luasnippets/r.lua
@@ -26,6 +26,13 @@ fmta([[<> = melt(<>, <>, <>, variable.name="<>", value.name="<>")]],
 {i(1, "dtm"), i(2, "dt"), i(3, "id.vars"), i(4, "measure.vars"), i(5, "variable"), i(6, "value")}),
 {condition=conds.line_begin}),
 
+
+-- data.table
+
+s({trig="update", dscr="Update joined rows"},
+fmta([[<>[<>, on=c(<>="<>"), c("<>") := .(<>)]
+]], {i(1, "DT_TRG"), i(2, "DT_SRC"), i(3, "ON_TRG"), i(4, "ON_SRC"), i(5, "COL_TRG"), i(6, "i.COL_DUP")})),
+
 -- ggplot
 
 s({trig="ggplot", dscr="ggplot", condition=conds.line_begin, snippetType="autosnippet"},
@@ -37,5 +44,9 @@ s({trig="expandy0", dscr="Remove the default empty space under y=0"},
 t"scale_y_continuous(expand=expansion(mult=c(0,.05)))", -- .05 based on ?expansion
 {condition=conds.line_begin}),
 
+-- altho you can annotate anything, not just text.
+s({trig="annotate", dscr="annotate text"},
+fmta([[annotate("text", label="<>", x=<>, y=<>, size=<>, hjust=0) +]],
+{i(1, "LABEL"), i(2, "x"), i(3, "y"), i(4, "4")})),
 
 }
