7efa2bb6e04abfa041b47fe7c030a67f2d0dc096
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-05-26 13:59:49 +1000

    use neovim api str split

--- lua/split.lua
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env lua
--- from https://stackoverflow.com/questions/1426954/split-string-in-lua
--- string split that splits a,b,,c -> a,b,"",c rather than typical suggestion where -> a,b,c
--- USE: for field in s:split('\t') do ... end 
--- e.g. out = {} then a number of calls to table.insert(out, field) to put in a vector (table).
-function string:split(pat)
-    return (self .. pat):gmatch("([^"..pat.."]*)"..pat)
-end
