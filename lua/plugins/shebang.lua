9d0a38938d3c9d52150e79bdf50fdb825ddc8531
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-07-12 23:50:28 +0200

    lazy load

--- lua/plugins/shebang.lua
+++ lua/plugins/shebang.lua
@@ -1,10 +1,12 @@
 -- insert shebang on new file edit
-return {"samirettali/shebang.nvim", config=function()
-    -- non-standard use of .sh extension
-    vim.g.shebang_commands = {
-        sh = '/usr/bin/env zsh',
-        zsh = '/usr/bin/env zsh',
-        R = '/usr/bin/env Rscript'
-    }
-end
+return {
+    "samirettali/shebang.nvim",
+    event = "InsertEnter",
+    config=function()
+        vim.g.shebang_commands = {
+            sh = '/usr/bin/env zsh', -- non-standard use of .sh extension
+            zsh = '/usr/bin/env zsh',
+            R = '/usr/bin/env Rscript'
+        }
+    end
 }
