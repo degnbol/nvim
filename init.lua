1ef78bd3738eec5581442737b027793053deb414
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-07-12 23:53:56 +0200

    default colorscheme on new install

--- init.lua
+++ init.lua
@@ -1,16 +1,14 @@
-require 'options'
+require "options"
 
--- lazy-load lazy.nvim
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {"git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  }
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, }
 end
 vim.opt.rtp:prepend(lazypath)
 require'lazy'.setup('plugins', {
+    -- https://github.com/folke/lazy.nvim
     change_detection = { notify = false, },
     dev = {
         -- directory where you store your local plugin projects
@@ -19,8 +17,9 @@ require'lazy'.setup('plugins', {
         patterns = {"degnbol"},
         fallback = true, -- Fallback to git when local plugin doesn't exist
     },
+    install = {colorscheme = {"default"}}, -- Don't switch colorscheme after startup install
 })
 
-require 'highlights'
-require 'keymap'
+require "highlights"
+require "keymap"
 
