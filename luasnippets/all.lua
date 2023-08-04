c514545e968b1c8f95ad0e011712b2c5a6ab2c2e
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-08-04 09:28:00 +0200

    trying out explicit triple press { to get brackets with newline

--- luasnippets/all.lua
+++ luasnippets/all.lua
@@ -1,3 +1,5 @@
+local util = require "utils/luasnip"
+
 return {
 -- for all filetypes
 
@@ -10,9 +12,19 @@ s({trig="``", dscr="autopairs", snippetType='autosnippet'},
 {t"`", i(1), t"`"}),
 s({trig="((", dscr="autopairs", snippetType='autosnippet', wordTrig=false},
 {t"(", i(1), t")"}),
-s({trig="{{", dscr="autopairs", snippetType='autosnippet', wordTrig=false},
+s({trig="{{", dscr="autopairs", snippetType='autosnippet', wordTrig=false, priority=100},
 {t"{", i(1), t"}"}),
+s({
+    trig='{{}',
+    dscr="Tripple press open bracket. Second bracket is typed last. The last one turns the other way due to the snippet above that turns {{ into {}",
+    snippetType='autosnippet',
+    trigEngine=function (trigger) return util.match_oneAhead end,
+},
+-- NOTE: there is no final bracket in replacement since one is left over from end of trigger.
+fmta([[{
+    <>
 
+]], i(1))),
 
 
 }
