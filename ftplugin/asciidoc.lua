6a5447d491058264b9b22d10475fca1ac92fcfd9
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-08-14 17:25:30 +0200

    goto tag definition for xref under cursor

---
+++ ftplugin/asciidoc.lua
@@ -0,0 +1,31 @@
+#!/usr/bin/env lua
+
+local function get_xref_id()
+    local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+    local c=c+1 -- 1-index
+    local line = vim.api.nvim_get_current_line()
+    -- check for <<id,text>> pattern
+    for startpos, xref_match, endpos in line:gmatch('()(%b<>)()') do
+        if startpos <= c and c <= endpos then
+            local tag = xref_match:match('^<<([a-z-_]+).*>>$')
+            return tag
+        end
+    end
+    -- check for xref:id[text] pattern
+    for startpos, tag, endpos in line:gmatch('()xref:([a-z-_]+)%b[]()') do
+        if startpos <= c and c <= endpos then
+            return tag
+        end
+    end
+end
+
+-- Goto tag that xref points to under cursor
+function goto_xref_tag()
+    local tag = get_xref_id()
+    if tag then
+        vim.cmd('tag ' .. tag)
+    end
+end
+
+vim.keymap.set('n', 'gd', goto_xref_tag, { desc="Goto tag definition" })
+
