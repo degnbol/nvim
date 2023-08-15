818fd54745953af52353a7383173a099f9c38fe2
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-08-15 15:45:09 +0200

    exception for fname starting with '.'

--- ftdetect/noext.lua
+++ ftdetect/noext.lua
@@ -1,25 +1,18 @@
-api = vim.api
-cmd = vim.cmd
-fn = vim.fn
 -- If a new file is opened without extension, assume it is an incomplete 
 -- tab-completion match. We deal with it by opening the parent folder and 
 -- moving the cursor to the first match. Since we use BufNewFile this won't be 
 -- a problem opening an already existing file named without extension.
--- If you really wish to make a file without extension then simply use `touch` first.
-api.nvim_create_autocmd({"BufNewFile"}, {
-    pattern="*",
-    callback=function()
-        if fn.expand('%:e') == '' then
-            fname = fn.expand('%:t')
-            folder = fn.expand('%:h')
-            cmd 'bdel'
-            cmd("e " .. folder)
-            -- we need to let the tree explorer plugin open the window before we can search.
-            vim.wait(1)
-            -- start search string with a space since there is a space in my 
-            -- explorer before file names so we match filenames from their 
-            -- start (trying to do a ^ pattern)
-            cmd('silent! / ' .. fname)
+-- If you really wish to make a file without extension then simply use `touch` 
+-- first or specify filename after opening nvim.
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern="*", callback=function()
+        local ext = vim.fn.expand('%:e')
+        local fname = vim.fn.expand('%:t')
+        -- make exception for filename that starts with '.', e.g. .gitignore, 
+        -- since this is not a mistake even though it doesn't have an 
+        -- extension.
+        if ext == '' and fname:sub(1, 1) ~= '.' then
+            vim.cmd "q"
         end
     end
 })
