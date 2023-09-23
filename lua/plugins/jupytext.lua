e9f98db958a4f0e3ffb03eea94ea51dc0651ab86
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-09-23 15:34:36 +1000

    viml -> lua

--- lua/plugins/jupytext.lua
+++ lua/plugins/jupytext.lua
@@ -1,9 +1,13 @@
 #!/usr/bin/env lua
 -- edit jupyter notebook. requires `pip install jupytext`
-return {"goerz/jupytext.vim", config=function()
-    -- conversion settings
-    vim.cmd 'let g:jupytext_fmt = "py:percent"'
-    -- highlight blocks
-    vim.cmd 'match Block /^# %%/'
-    vim.cmd 'hi link Block LineNr'
-end}
+return {
+    "goerz/jupytext.vim",
+    config=function()
+        -- conversion settings
+        vim.g.jupytext_fmt = "py:percent"
+        -- highlight blocks.
+        -- Doesn't work since treesitter trumps. Need to add a custom query.
+        vim.cmd 'syn match Block /^# %%/'
+        vim.api.nvim_set_hl(0, "Block", {link="LineNr"})
+    end,
+}
