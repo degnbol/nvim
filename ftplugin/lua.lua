6f6533175f58e3c1e98de1bc4bad26ece673c1f4
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-09-29 12:27:02 +1000

    gf for lua

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -8,3 +8,8 @@ require 'luasnippets/lazy'
 -- use :help instead of default Man for visual selection in lua
 vim.keymap.set('x', 'K', [["hy:h <C-r>h<CR>]], { buffer=true, desc="Help" })
 
+-- when doing gf or similar obviously we should look in the lua/ folder since 
+-- this is where all scripts are required from.
+local rtp = vim.opt.runtimepath:get()[1]
+vim.opt_local.path:append(rtp .. "/lua")
+
