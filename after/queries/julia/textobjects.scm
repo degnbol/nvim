72be02134811d6a4c8428742d194a40e978721cf
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-10-01 21:54:01 +1100

    fixed ia text arg in julia

---
+++ after/queries/julia/textobjects.scm
@@ -0,0 +1,11 @@
+;extends
+(argument_list (_) @parameter.inner)
+
+; parameters are really what goes in e.g. function definition and arguments are 
+; when they are called but we are mixing it all together as parameters for a 
+; simple single textobj to call with e.g. ]a, <leader>a<left>, via etc.
+; parameter_list may have the child keyword_parameters which we shouldn't 
+; capture, so I'm writing out all other options. i.e. I'm trying to do all 
+; minus keyword_parameters.
+(parameter_list (identifier) (typed_parameter) (optional_parameter) @parameter.inner)
+(keyword_parameters (_) @parameter.inner)
