2a997e2a97226c4386bbdf2ed8648f26ca1cd9ad
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-10-08 22:18:08 +1100

    Jump forward and backward one split
    Hacky topline setter solution replaced with a decent solution, calling
    vimscript.
    
    Made a keybind to jump forward and backward one window height, so it
    looks like the splits have their content shiftet one to the left or
    right.

--- lua/guitar/vnewFurther.lua
+++ lua/guitar/vnewFurther.lua
@@ -1,24 +1,31 @@
---- Hacky solution to set a window offset in a buffer so that a given integer 
---- row in the buffer appears the as topline in the window.
-local function setWinTopline(win, topline)
-    scrolloff = vim.opt_local.scrolloff:get()
-    height = vim.api.nvim_win_get_height(win)
-    lastline = vim.fn.line "$"
-    vim.api.nvim_win_set_cursor(win, {math.min(topline+height-scrolloff, lastline), 0})
-    vim.api.nvim_win_set_cursor(win, {math.min(topline+scrolloff, lastline), 0})
+local function getTopline()
+    return vim.fn.line("w0")
+end
+
+local function setTopline(topline)
+    vim.fn.winrestview {topline=topline, lnum=topline+vim.opt_local.scrolloff:get()}
 end
 
 function vnewFurther()
     for i = 1, math.max(vim.v.count,1) do
         win = vim.api.nvim_get_current_win()
         vim.opt_local.scrollbind = true
-        topline = vim.fn.line("w0")
+        topline = getTopline()
         height = vim.api.nvim_win_get_height(win)
         vim.cmd.vnew "%"
-        setWinTopline(0, topline+height)
+        setTopline(topline+height)
         vim.opt_local.scrollbind = true
     end
 end
 
+function forwardScreen()
+    setTopline(getTopline() + vim.api.nvim_win_get_height(win))
+end
+function backwardScreen()
+    setTopline(getTopline() - vim.api.nvim_win_get_height(win))
+end
+
 vim.keymap.set('n', '<leader><leader>v', vnewFurther, { desc="vnew with one window height offset and scrollbind enabled. Accepts a count." })
+vim.keymap.set('n', '<leader><leader>f', forwardScreen, { desc="Like <C-f> but jumps foward one screen visually in the split regardless of scrolloff." })
+vim.keymap.set('n', '<leader><leader>b', backwardScreen, { desc="Like <C-b> but jumps backwards one screen visually in the split regardless of scrolloff." })
 
