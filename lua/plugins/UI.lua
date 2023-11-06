94ab74c53210a2e93e0a9c805a3e5800b15c227e
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-11-06 16:33:17 +1100

    Add number of hidden lines to folded virt text

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -30,7 +30,7 @@ return {
                 editPaste="<leader>re",
             },
             exclude = {tex=true, text=true, tsv=true, markdown=true},
-            progress = false,
+            progress = true,
             editpaste = true,
             closepager = true,
         },
@@ -50,7 +50,40 @@ return {
         vim.keymap.set('n', 'zR', require('ufo').openAllFolds, {desc="Open all folds"})
         vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, {desc="Close all folds"})
 
-        require('ufo').setup()
+        -- vim.api.nvim_set_hl(0, "UfoFoldedEllipsis", {link="Statement", default=true})
+        -- vim.cmd "hi! default link UfoFoldedEllipsis Statement"
+
+        require('ufo').setup {
+            open_fold_hl_timeout = 0, --disable hl
+            close_fold_kinds = {},
+            fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+                local newVirtText = {}
+                local suffix = (' … %d'):format(endLnum - lnum)
+                local sufWidth = vim.fn.strdisplaywidth(suffix)
+                local targetWidth = width - sufWidth
+                local curWidth = 0
+                for _, chunk in ipairs(virtText) do
+                    local chunkText = chunk[1]
+                    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    if targetWidth > curWidth + chunkWidth then
+                        table.insert(newVirtText, chunk)
+                    else
+                        chunkText = truncate(chunkText, targetWidth - curWidth)
+                        local hlGroup = chunk[2]
+                        table.insert(newVirtText, {chunkText, hlGroup})
+                        chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                        -- str width returned from truncate() may less than 2nd argument, need padding
+                        if curWidth + chunkWidth < targetWidth then
+                            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                        end
+                        break
+                    end
+                    curWidth = curWidth + chunkWidth
+                end
+                table.insert(newVirtText, {suffix, 'NonText'})
+                return newVirtText
+            end
+        }
     end},
     -- file explorer as a buffer
     {"stevearc/oil.nvim",
