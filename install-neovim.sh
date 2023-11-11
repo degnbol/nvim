848213a81bbed48d3f715558f0de6a8fd5092373
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-11-11 12:05:52 +1100

    unlink before reinstall

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 if [ `uname` = "Darwin" ]; then
     # --HEAD for development version instead of stable necessary for some lua config
-    brew install --HEAD luajit
+    brew install --HEAD luajit || brew unlink luajit && brew install --HEAD luajit
     brew install --HEAD neovim
     brew install npm # install npm for :LspInstall python that install python support for Lsp. 
     npm install -g neovim # if I do :checkhealth it makes a warning recommending to install this so I did
