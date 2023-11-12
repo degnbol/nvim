9c82d8fa29b66bf7660c7453cda1927f7681e53d
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-11-13 00:41:56 +1100

    +keybind move bufferline tab

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,15 +7,17 @@ return {
         -- mode for something related to completion.
         -- vim.keymap.set("n", "<TAB>", "<Cmd>BufferLineCycleNext<CR>", {desc="Next buffer"})
         -- vim.keymap.set("n", "<S-TAB>", "<Cmd>BufferLineCyclePrev<CR>", {desc="Previous buffer"})
-        vim.keymap.set("n", "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", {desc="Buffer 1"})
-        vim.keymap.set("n", "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>", {desc="Buffer 2"})
-        vim.keymap.set("n", "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>", {desc="Buffer 3"})
-        vim.keymap.set("n", "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>", {desc="Buffer 4"})
-        vim.keymap.set("n", "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>", {desc="Buffer 5"})
-        vim.keymap.set("n", "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>", {desc="Buffer 6"})
-        vim.keymap.set("n", "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>", {desc="Buffer 7"})
-        vim.keymap.set("n", "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>", {desc="Buffer 8"})
-        vim.keymap.set("n", "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>", {desc="Buffer 9"})
+        vim.keymap.set("n", "<leader>1",       "<Cmd>BufferLineGoToBuffer 1<CR>", {desc="Buffer 1"})
+        vim.keymap.set("n", "<leader>2",       "<Cmd>BufferLineGoToBuffer 2<CR>", {desc="Buffer 2"})
+        vim.keymap.set("n", "<leader>3",       "<Cmd>BufferLineGoToBuffer 3<CR>", {desc="Buffer 3"})
+        vim.keymap.set("n", "<leader>4",       "<Cmd>BufferLineGoToBuffer 4<CR>", {desc="Buffer 4"})
+        vim.keymap.set("n", "<leader>5",       "<Cmd>BufferLineGoToBuffer 5<CR>", {desc="Buffer 5"})
+        vim.keymap.set("n", "<leader>6",       "<Cmd>BufferLineGoToBuffer 6<CR>", {desc="Buffer 6"})
+        vim.keymap.set("n", "<leader>7",       "<Cmd>BufferLineGoToBuffer 7<CR>", {desc="Buffer 7"})
+        vim.keymap.set("n", "<leader>8",       "<Cmd>BufferLineGoToBuffer 8<CR>", {desc="Buffer 8"})
+        vim.keymap.set("n", "<leader>9",       "<Cmd>BufferLineGoToBuffer 9<CR>", {desc="Buffer 9"})
+        vim.keymap.set("n", "<leader>b<left>", "<Cmd>BufferLineMovePrev<CR>",     {desc="Move left"})
+        vim.keymap.set("n", "<leader>b<right>","<Cmd>BufferLineMoveNext<CR>",     {desc="Move right"})
     end,
     opts = {
         options = {
