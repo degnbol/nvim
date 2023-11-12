31919afdf107d75a948a2a40592f2c4c05811029
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-11-13 00:41:16 +1100

    toggle saving on type

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,3 +1,36 @@
 -- assume using typst.vim
 vim.keymap.set('n', '<leader>cc', "<Cmd>TypstWatch<CR>", { desc="Compile continuously", buffer=true })
 vim.keymap.set('n', '<leader>cv', "!open -a sioyek<CR>", { desc="Compile view", buffer=true, silent=true, })
+
+-- toggle updating pdf on typing.
+-- Doesn't work if running module file, hence we use TypstWatch instead by default.
+vim.keymap.set('n', '<leader><leader>t', function ()
+    local lsp = require"lspconfig".typst_lsp
+    if lsp.manager.config.settings.exportPdf == "onType" then
+        exportPdf = "never" -- use TypstWatch to compile
+    else
+        exportPdf = "onType"
+    end
+    vim.cmd.LspStop()
+    require"lspconfig".typst_lsp.setup { settings = { exportPdf = exportPdf } }
+    vim.cmd.LspStart()
+    print(exportPdf)
+end, { desc="Toggle update on type" })
+
+vim.api.nvim_set_hl(0, "typstMarkupHeading", {link="@text.title", default=true})
+-- Semantic tokens.
+-- captures \ at the end of lines, ~ between words and \* that let's you write a literal *. The last one is not ideal but worth it for the other two.
+vim.api.nvim_set_hl(0, "@lsp.type.escape.typst", {link="@comment", default=true})
+vim.api.nvim_set_hl(0, "@lsp.typemod.punct.emph.typst", {link="@comment", default=true})
+vim.api.nvim_set_hl(0, "@lsp.typemod.punct.strong.typst", {link="@comment", default=true})
+vim.api.nvim_set_hl(0, "@lsp.type.punct.typst", {link="RainbowDelimiterRed", default=true})
+vim.api.nvim_set_hl(0, "@lsp.type.pol.typst", {link="@variable", default=true})
+
+-- local grp = vim.api.nvim_create_augroup("typst", {clear=true})
+-- vim.api.nvim_create_autocmd("Colorscheme", {
+--     buffer = true,
+--     group = grp,
+--     callback = function ()
+--        
+--     end
+-- })
