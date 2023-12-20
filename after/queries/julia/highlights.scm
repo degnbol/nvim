a505c3f35829244ef3e681d2b51860da05b868a5
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-12-20 23:07:36 +1100

    fix julia pipe to func TS query

--- after/queries/julia/highlights.scm
+++ after/queries/julia/highlights.scm
@@ -1,2 +1,11 @@
 ;extends
 "in" @repeat
+
+; fix syntax thinking `x |> f` is piping into a variable.
+(binary_expression
+  (_)
+  (operator) @pipe
+  (identifier) @function.call
+  (#lua-match? @pipe "|>"))
+; NOTE: has to be lua-match and not match, since the latter will use | as OR operator.
+
