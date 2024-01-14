0615a64496c638e13b7393cccd6de2c8ad30a477
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-01-14 20:16:21 +1100

    concise

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,3 +1,5 @@
+local hl = require "utils/highlights"
+
 -- assume using typst.vim
 vim.keymap.set('n', '<leader>cc', "<Cmd>TypstWatch<CR>", { desc="Compile continuously", buffer=true })
 vim.keymap.set('n', '<leader>cv', "!open -a sioyek<CR>", { desc="Compile view", buffer=true, silent=true, })
@@ -17,26 +19,19 @@ vim.keymap.set('n', '<leader><leader>t', function ()
     print(exportPdf)
 end, { desc="Toggle update on type" })
 
-vim.api.nvim_set_hl(0, "typstMarkupHeading", {link="@text.title", default=true})
+hl.def("typstMarkupHeading", "@text.title")
 -- Semantic tokens.
 -- captures \ at the end of lines, ~ between words and \* that let's you write a literal *. The last one is not ideal but worth it for the other two.
-vim.api.nvim_set_hl(0, "@lsp.type.escape.typst", {link="@comment", default=true})
+hl.def("@lsp.type.escape", "@comment")
 -- undo coloring from above on "..." with help from custom capture in after/queries/typst/highlights.scm
-vim.api.nvim_set_hl(0, "@punct.ellipsis.typst", {link="Normal", default=true})
-vim.api.nvim_set_hl(0, "@lsp.typemod.punct.emph.typst", {link="@comment", default=true})
-vim.api.nvim_set_hl(0, "@lsp.typemod.punct.strong.typst", {link="@comment", default=true})
-vim.api.nvim_set_hl(0, "@lsp.type.punct.typst", {link="Delimiter", default=true})
-vim.api.nvim_set_hl(0, "@lsp.type.pol.typst", {link="@variable", default=true})
-vim.api.nvim_set_hl(0, "@lsp.type.string.typst", {link="@string", default=true})
-
--- local grp = vim.api.nvim_create_augroup("typst", {clear=true})
--- vim.api.nvim_create_autocmd("Colorscheme", {
---     buffer = true,
---     group = grp,
---     callback = function ()
---        
---     end
--- })
+hl.def("@punct.ellipsis", "Normal")
+hl.def("@lsp.typemod.punct.emph", "@comment")
+hl.def("@lsp.typemod.punct.strong", "@comment")
+hl.def("@lsp.type.punct", "Delimiter")
+hl.def("@lsp.type.pol", "@variable")
+hl.def("@lsp.type.string", "@string")
+hl.def("@lsp.type.heading", "@text.title")
+hl.def("@lsp.type.number", "@number")
 
 vim.opt_local.wrap = true
 vim.opt_local.sidescrolloff = 0
