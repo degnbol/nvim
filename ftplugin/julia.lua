bae1affbc735b90d67ec7a910a1da067e24418fe
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-01-15 19:38:39 +1100

    update julia indent comment

--- ftplugin/julia.lua
+++ ftplugin/julia.lua
@@ -6,15 +6,7 @@ vim.opt.conceallevel = 1
 -- you have to be in insert mode to unconceal
 vim.opt.concealcursor = "nvc"
 
--- when treesitter runs for julia the indent is wrong after inline for loop,
--- but setting the indentexpr to the one from treesitter it works, but then it
--- breaks indents after end of function.
--- The problem with indent happening when writing a bracket is actually due to
--- indentkeys which are a set of keys that when written will trigger autoindent
--- of a line. Setting the indent of a line is ALWAYS done by calling
--- indentexpr which is GetJuliaIndent() by default. 
--- Setting the treesitter config enable flag sets indentexpr=nvim_treesitter#indent()
--- I think I finally found the solution to fix indent for julia.
+-- A fallback if indent is bad in Julia.
 -- Simply use the builtin smartindent instead of indentexpr.
 -- UNLESS the additional_vim_regex_highlighting option is set for treesitter, then use GetJuliaIndent.
 -- vim.opt.indentexpr = "" -- is set in after/syntax/julia.vim since this dir isn't late enough
@@ -23,10 +15,4 @@ vim.opt.concealcursor = "nvc"
 vim.opt.list = false
 
 require"completion/plotlyjs".setup()
-
--- syntax is too slow on julia, e.g. for sidescroll.
--- We don't need it, we have treesitter.
-vim.schedule(function ()
-    vim.opt_local.syntax = "off"
-end)
 
