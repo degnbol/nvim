33c968eef8924c4497fcace46a9e67181dbe9891
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-01-28 21:13:59 +1100

    install from source?

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env zsh
-if [ `uname` = "Darwin" ]; then
-    # --HEAD for development version instead of stable necessary for some lua config
-    brew install --HEAD luajit || brew unlink luajit && brew install --HEAD luajit
-    brew install --HEAD neovim
-    brew install npm # install npm for :LspInstall python that install python support for Lsp. 
-    npm install -g neovim # if I do :checkhealth it makes a warning recommending to install this so I did
-else
-    cd ~/bin
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-    tar xzvf nvim-linux64.tar.gz
-    ln -s ~/bin/nvim-linux64/bin/nvim ~/bin/nvim
-    rm nvim-linux64.tar.gz
-fi
-
+cd $XDG_CONFIG_HOME/../neovim/
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+# assuming bin/ and dotfiles/ are in ~
+cd ~/bin/
+ln -s ../dotfiles/neovim/build/bin/nvim .
