2c0d580794cc5197c191894c85452af9ccc68265
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-03-20 18:08:48 +1100

    visible broadcast dot

--- after/queries/julia/highlights.scm
+++ after/queries/julia/highlights.scm
@@ -1,20 +1,6 @@
 ;extends
 
-
 "in" @repeat
-
-; fix syntax thinking `x |> f` is piping into a variable.
-; NOTE: has to be "lua-match" and not "match", since the latter will use | as OR operator.
-(binary_expression
-  (_)
-  (operator) @_pipe
-  (identifier) @function.call
-  (#any-of? @_pipe "|>" ".|>"))
-; TODO: maybe figure out how to set this as lower priority
-; (tried adding (#set! "priority" 0))
-; Since piping into types (e.g. x |> String) has the right highlights
-; (same as String(x)) but in the wrong order, so the color from builtin type is 
-; overruled by function.call.
 
 ; support @chain macro recognizing @function.call as well.
 (macrocall_expression
@@ -34,4 +20,8 @@
 
 ; (catch_clause
 ;   ";" @punctuation.delimiter)
+
+; more focus to the dot that indicates a broadcast
+(broadcast_call_expression
+  "." @punctuation.special)
 
