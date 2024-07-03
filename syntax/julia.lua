20da3a4063ee2a34ce59721e00049528cbacf414
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-07-03 18:10:58 +0100

    highlight julia comma and semicolon as delim

--- syntax/julia.lua
+++ syntax/julia.lua
@@ -3,7 +3,10 @@ local hl = require "utils/highlights"
 -- Syntax highlights provide JuliaSemicolon, JuliaOperator, etc.
 -- We currently enable it since treesitter doesn't capture ; as expression delimiter.
 hl.def("JuliaSemicolon", "@punctuation.delimiter")
+hl.def("JuliaComma", "@punctuation.delimiter")
 -- We disable JuliaOperator since it captures ...
 -- Syntax highlights are too crude and doesn't have punctuation as a concept.
 -- We prefer @punctuation.special for ... since operator will highlight the dots like a broadcast dot, and they are very different.
 hl.def("JuliaOperator", "Normal")
+hl.def("JuliaTypeOperator", "Normal")
+
