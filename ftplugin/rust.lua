56f635abbae08e5efd2d5a565fc45373c747453c
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-08-15 11:43:40 +1000

    ft specific keymaps

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,4 +1,8 @@
 #!/usr/bin/env lua
-vim.keymap.set('n', '<leader>cc', ":Cargo run\n", { desc="Cargo run" })
-vim.keymap.set('n', '<leader>cb', ":Cargo build\n", { desc="Cargo build" })
-vim.keymap.set('n', '<leader>cC', ":Cargo check\n", { desc="Cargo check" })
+vim.keymap.set('n', '<leader>cc',        ":Crun\n",   { desc="Cargo run" })
+vim.keymap.set('n', '<leader><leader>r', ":Crun\n",   { desc="Cargo run" })
+vim.keymap.set('n', '<leader><leader>b', ":Cbuild\n", { desc="Cargo build" })
+vim.keymap.set('n', '<leader><leader>c', ":Ccheck\n", { desc="Cargo check" })
+vim.keymap.set('n', '<leader><leader>C', ":Cclean\n", { desc="Cargo clean" })
+vim.keymap.set('n', '<leader><leader>t', ":Ctest\n",  { desc="Cargo test" })
+vim.keymap.set('n', '<leader><leader>u', ":Cupdate\n",{ desc="Cargo update" })
