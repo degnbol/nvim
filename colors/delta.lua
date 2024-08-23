360366e9f40bcce7e1697a01d43f7a493553d8c3
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-08-23 19:47:34 +1000

    hl of @string.special changed with BC

--- colors/delta.lua
+++ colors/delta.lua
@@ -11,9 +11,6 @@ require('fluoromachine.config').load()
 
 -- visual selection is not clear enough.
 hi.bg("Visual", "#6a3b6a")
--- default is the same color as functions, but @string.special is the only 
--- special hl with a different color.
-hi.link("Special", "@string.special")
 
 -- It's green by default which I associate with line added
 hi.set("CursorLineNr", {fg="white", bold=true})
