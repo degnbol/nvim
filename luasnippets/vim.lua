a4370804c7d8597f8678fa0d66424d6db667cdb7
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-09-24 14:39:26 +1000

    syntax hl snip

--- luasnippets/vim.lua
+++ luasnippets/vim.lua
@@ -7,6 +7,36 @@ return {
 s({trig="esc", dscr="Temp ESC"},
 {t[[<C-\><C-O>]]}),
 
+s({trig="syntax", dscr="Syntax capture.", trigEngine="pattern", show_condition=conds.line_end},
+{
+    t"syntax ",
+    c(1, {t"match", t"region"}),
+    t" ",
+    i(2, "HLGROUP"),
+    t" ",
+    d(3, function (args)
+        if args[1][1] == "match" then
+            return sn(nil, { -- the returned snippetNode doesn't need a position; it's inserted "inside" the dynamicNode.
+                t"/",
+                -- jump-indices are local to each snippetNode, so restart at 1.
+                i(1, "PATTERN"),
+                t"/",
+            })
+        else
+            return sn(nil, { -- the returned snippetNode doesn't need a position; it's inserted "inside" the dynamicNode.
+                t"start=/",
+                -- jump-indices are local to each snippetNode, so restart at 1.
+                i(1, "PATTERN"),
+                t"/ end=/",
+                i(2, "PATTERN"),
+                t"/",
+                c(3, {t"", {t" matchgroup=", i(1, "HLSTARTEND")}})
+            })
+        end
+    end, {1}),
+    c(4, {t"", {t" containedin=", i(1, "A,B,C")}})
+}),
+
 }, {
 -- auto
 
