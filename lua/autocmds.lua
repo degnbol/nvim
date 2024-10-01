17e162d38ca20f34ca787bc703e83957c15ceaa5
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-10-01 21:14:13 +1000

    default lines of different filetypes

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,11 +1,29 @@
 #!/usr/bin/env lua
--- autocmd to extend a query file by default.
-local grp = vim.api.nvim_create_augroup("Extends", {clear=true})
-vim.api.nvim_create_autocmd("BufNewFile", {
-    pattern = "*.scm",
-    group = grp,
-    callback = function ()
-        vim.api.nvim_put({";extends"}, "l", false, true)
-    end
-})
+-- default contents in new files of specific types
+local defaultlines = {
+    scm = {
+        ";extends",
+    },
+    R = {
+        "suppressPackageStartupMessages(library(data.table))",
+        "suppressPackageStartupMessages(library(ggplot2))",
+    },
+    py = {
+        "import numpy as np",
+    },
+    jl = {
+        "using DataFrames, CSV",
+    },
+}
 
+local grp = vim.api.nvim_create_augroup("defaultfile", {clear=true})
+
+for ext, lines in pairs(defaultlines) do
+    vim.api.nvim_create_autocmd("BufNewFile", {
+        pattern = "*." .. ext,
+        group = grp,
+        callback = function ()
+            vim.api.nvim_put(lines, "l", false, true)
+        end
+    })
+end
