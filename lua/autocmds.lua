95000bf9f5eac1f0e1ce47db083306777fc0c110
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-10-17 19:55:27 +1100

    usage default lines

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/env lua
--- default contents in new files of specific types
+-- default contents in new files of specific types.
+-- Either lines, or function producing lines
 local defaultlines = {
     scm = {
         ";extends",
@@ -14,16 +15,43 @@ local defaultlines = {
     jl = {
         "using DataFrames, CSV",
     },
+    -- complgen in config/complgen/*.usage
+    usage = function ()
+        -- assume filename is command name
+        local cmdname = vim.fn.expand('%:r')
+        lines = {
+            "# || syntax allows <PATH> to take completion precidence over options (otherwise they would be given together)",
+            cmdname .. " <PATH> || [<OPTION>]... <PATH>;",
+            "",
+            "<OPTION> ::=",
+            "      (--help) \"display this help and exit\"",
+            "    | (--version) \"display PyMOL version and exit\"",
+            "    ;",
+            "",
+            "# Fix PATH not adding space after completion.",
+            [[<PATH@zsh> ::= {{{ _path_files | sed '/\/$/!s/$/ /' }}};]],
+        }
+        return lines
+    end,
 }
 
 local grp = vim.api.nvim_create_augroup("defaultfile", {clear=true})
 
 for ext, lines in pairs(defaultlines) do
-    vim.api.nvim_create_autocmd("BufNewFile", {
-        pattern = "*." .. ext,
-        group = grp,
+    local callback
+    if type(lines) == "function" then
+        callback = function ()
+            vim.api.nvim_put(lines(), "l", false, true)
+        end
+    else
         callback = function ()
             vim.api.nvim_put(lines, "l", false, true)
         end
+    end
+    vim.api.nvim_create_autocmd("BufNewFile", {
+        pattern = "*." .. ext,
+        group = grp,
+        callback = callback
     })
 end
+
