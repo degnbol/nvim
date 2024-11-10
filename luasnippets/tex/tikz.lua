edeb76f2b66b010a10b7efdc916fc8448d0fe130
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-11-10 17:43:52 +1100

    tikz snippet init

---
+++ luasnippets/tex/tikz.lua
@@ -0,0 +1,12 @@
+#!/usr/bin/env lua
+-- TODO: only complete in tikz env
+return {
+s({trig="node", dscr="tikz node"},
+fmta([[\node<><> at (<>) {<>};
+]], {
+    i(1, " (NEW ID)"),
+    i(2, " [blue,fill=red]"),
+    i(3, "0,0"),
+    i(4, "TEXT"),
+})),
+}
