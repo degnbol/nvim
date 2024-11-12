716d84ec8338c56611cbd4bc89aa8c677ce89fa3
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-11-12 20:15:28 +1100

    quicker layout change

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -264,10 +264,10 @@ vim.keymap.set("n", "]4", "<Plug>(vimtex-]n)", { buffer=true, desc="Math start" 
 vim.keymap.set("n", "]$", "<Plug>(vimtex-]N)", { buffer=true, desc="Math end" })
 
 vim.keymap.set('n', '<leader><leader>2', function ()
-    os.execute("rectangle Skim right-half && rectangle kitty left-half")
+    os.execute("rectangle Skim right-half kitty left-half")
 end, { desc="Half screen layout" })
 vim.keymap.set('n', '<leader><leader>3', function ()
-    os.execute("rectangle Skim last-third && rectangle kitty first-two-thirds")
+    os.execute("rectangle Skim last-third kitty first-two-thirds")
 end, { desc="Third screen layout" })
 
 -- colorscheme aucmd to fix missing or inconsistent hl links
