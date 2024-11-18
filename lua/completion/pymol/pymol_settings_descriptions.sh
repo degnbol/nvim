dc14442dcbd8e7b902c5a695e5560273a219b414
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-11-18 17:39:13 +1100

    clean

--- lua/completion/pymol/pymol_settings_descriptions.sh
+++ lua/completion/pymol/pymol_settings_descriptions.sh
@@ -7,3 +7,6 @@ cat ./pymol_settings.txt | while read line; do
     wget -O temp.html "https://pymolwiki.org/index.php/$line"
     grep -A2 'id="Overview"' temp.html | grep -A2 '<p>' | grep -B2 '<\p>' > ./pymol_settings_descriptions/$line.txt
 done
+
+# cleanup
+rm temp.html
