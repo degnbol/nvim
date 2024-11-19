f3aed805b5e6353ad983d3efb205ed91139fce67
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-11-19 16:23:23 +1100

    fixed textcolor hls

--- lua/tex/textcolor.lua
+++ lua/tex/textcolor.lua
@@ -3,11 +3,18 @@
 local ns = vim.api.nvim_create_namespace("textcolor")
 vim.api.nvim_set_hl_ns(ns)
 
-for _, col in ipairs({"red", "green", "blue", "magenta", "cyan", "yellow", "purple"}) do
-    vim.api.nvim_set_hl(ns, col, {fg=col})
+local colors = {"gray", "red", "green", "blue", "magenta", "cyan", "yellow", "purple"}
+local fgs = {}
+for _, col in ipairs(colors) do
+    fgs[col] = col
+    -- add yellowtext etc. which are (usually) darker variants made for being able to read the text on a white paper background.
+    fgs[col .. "text"] = col
+end
+-- make hl groups
+for col, fg in pairs(fgs) do
+    vim.api.nvim_set_hl(ns, col, {fg=fg})
 end
 
--- TODO: use maybe vim.api.nvim_buf_clear_namespace to clear highlights if we change textcolor in a textcolor cmd
 local function textcolorRange(buf, linestart, lineend)
     local lines = vim.api.nvim_buf_get_lines(buf, linestart, lineend, false) -- true errors on new empty file
     if table.concat(lines):match('\\textcolor') then
@@ -16,8 +23,10 @@ local function textcolorRange(buf, linestart, lineend)
             local ln = linestart+iLine-1
             -- may have multiple matches on a single line
             for col, c1, content in line:gmatch('\\textcolor{(%a+)}()(%b{})') do
-                -- -1=zero-index
-                vim.api.nvim_buf_add_highlight(buf, ns, col, ln, c1-1, c1+#content-1)
+                if fgs[col] ~= nil then
+                    -- -1=zero-index
+                    vim.api.nvim_buf_add_highlight(buf, ns, col, ln, c1, c1+#content-2)
+                end
             end
         end
     end
@@ -25,6 +34,7 @@ end
 
 vim.api.nvim_buf_attach(0, false, {
     on_lines=function (_, buf, changedtick, linenum0, linenum1, linenumLast, preCount, del_codepoints, del_codeunits)
+        vim.api.nvim_buf_clear_namespace(buf, ns, linenum0, math.max(linenum1, linenumLast))
         textcolorRange(buf, linenum0, math.max(linenum1, linenumLast)+1)
     end,
     on_reload=function (_, buf)
