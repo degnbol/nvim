901447c430670c0b6a3817780f42f33af42b7fe2
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2025-07-30 11:07:56 +0100

    excl build/ from references

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,26 +1,25 @@
-
 local util = require "utils/init"
 
 -- remove o, we want to continue comments while editing them only (r).
--- no t and having c+a means only comments are autoformatted. 
+-- no t and having c+a means only comments are autoformatted.
 -- However, a made comment reformat slow, so don't use by default.
 vim.opt.formatoptions = "jwcrql"
 vim.opt.concealcursor = ""
 vim.opt.list = false
 
-local grp = vim.api.nvim_create_augroup("colorscheme", {clear=true})
+local grp = vim.api.nvim_create_augroup("colorscheme", { clear = true })
 vim.api.nvim_create_autocmd("Colorscheme", {
     buffer = 0,
     group = grp,
-    callback = function ()
-        vim.api.nvim_set_hl(0, "@cell", {reverse=true})
+    callback = function()
+        vim.api.nvim_set_hl(0, "@cell", { reverse = true })
     end
 })
 
 local function load_pymol()
     -- load additional pymol syntax hl
     local rtp = vim.opt.runtimepath:get()[1]
-    vim.schedule(function ()
+    vim.schedule(function()
         vim.cmd.source(rtp .. "/syntax/python_pymol.vim")
     end)
     -- The above needs to be done for each new buffer but the following only needs to be run once,
@@ -36,7 +35,11 @@ local function load_pymol()
     end
 end
 -- manually load
-vim.keymap.set('n', '<localleader>+', load_pymol, { buffer=true, desc="Manually load pymol snippets+completion+syntax" })
+vim.keymap.set('n', '<localleader>+', load_pymol, {
+    buffer = true,
+    desc =
+    "Manually load pymol snippets+completion+syntax"
+})
 -- check if pymol is loaded by scanning first 10 lines
 for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, 10, false)) do
     -- might be using e.g. `from pymol_util import *`
@@ -45,3 +48,20 @@ for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, 10, false)) do
     end
 end
 
+-- Filter the default goto references so we don't see the "build/" references.
+vim.keymap.set('n', 'grr', function()
+    vim.lsp.buf.references(nil, {
+        on_list = function(options)
+            local items = {}
+            for _, item in ipairs(options.items) do
+                -- :h setqflist-what
+                if not item.filename:match("build/") then
+                    table.insert(items, item)
+                end
+            end
+            options.items = items
+            vim.fn.setqflist({}, ' ', options)
+            vim.cmd('botright copen')
+        end
+    })
+end, { desc = "Goto references (excl build/)", buffer = true })
