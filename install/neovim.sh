c8c7a9c9ce328e4e1d12394a17ddb6acd331e3af
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2026-02-17 16:47:16 +0000

    ~/bin/ -> ~/.local/bin/

--- install/neovim.sh
+++ install/neovim.sh
@@ -3,10 +3,11 @@ if [ `uname` = "Darwin" ]; then
     brew install neovim npm # install npm for :LspInstall python that install python support for Lsp. 
     npm install -g neovim # if I do :checkhealth it makes a warning recommending to install this so I did
 else
-    cd ~/bin
+    mkdir -p ~/.local/bin ~/.local/share
+    cd ~/.local/share
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
     tar xzvf nvim-linux64.tar.gz
-    ln -s ~/bin/nvim-linux64/bin/nvim ~/bin/nvim
+    ln -s ~/.local/share/nvim-linux64/bin/nvim ~/.local/bin/nvim
     rm nvim-linux64.tar.gz
 fi
 
