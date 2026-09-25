bb356e876efeb960029224aa9f4fcce8c93b4bb8
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2026-09-25 09:20:16 +0100

    silence shellcheck on zsh which it doesn't support

--- after/lsp/bashls.lua
+++ after/lsp/bashls.lua
@@ -1,2 +1,6 @@
 
-return { filetypes = { "sh", "bash", "zsh", "sh.zsh" } }
+return {
+    filetypes = { "sh", "bash", "zsh", "sh.zsh" },
+    -- SC1071 ("only supports sh/bash/…") is shellcheck's only output for a zsh shebang.
+    settings = { bashIde = { shellcheckArguments = "-e SC1071" } },
+}
